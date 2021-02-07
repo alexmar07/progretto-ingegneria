17c8983598ed1d6b0085acf8ab7655fa52e6d01f
Mez-Bar <57267835+Mez-Bar@users.noreply.github.com>
2021-02-07 20:38:35 +0100

    Aggiunta Testing JUnit

---
+++ DesktopApp/src/sample/ServiceTest.java
@@ -0,0 +1,42 @@
+package sample;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    void doLoginTestSuccess() {
+        try {
+            Service.doLogin("administrator","password");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void doLoginTestUsernamePasswordErrati() {
+        try {
+            Service.doLogin("egewga","asfewehwe");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void doLoginTestUsernameValidoPasswordNonValida() {
+        try {
+            Service.doLogin("administrator","asfewehwe");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void doLoginTestUsernamePasswordNulli() {
+        try {
+            Service.doLogin("","");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
